844b4719a123aabafc068acc54a2ad9ca47b9ea2
KaioPio <164043787+KaioPio@users.noreply.github.com>
2024-06-15 17:16:57 -0300

    Add files via upload

---
+++ 2.2 - Tipos de dados/Atividade 1.py
@@ -0,0 +1,7 @@
+X = 2
+Y = 5
+print (type(X))
+print (type(Y))
+N4 = 5/2
+print (type(N4))
+print (N4)
---
+++ 2.2 - Tipos de dados/Atividade 2.py
@@ -0,0 +1,9 @@
+x = "O resultado é"
+x1 = 10
+x2 = 3.5
+x3 = x1 + x2
+print (type(x))
+print (type(x1))
+print (type(x2))
+print (type(x3))
+
